caec48a7cb743de4a20aef9268e8900cf520cf50
kozae00 <rhwogus777@gmail.com>
2025-02-19 22:58:09 +0900

    refactor : build.gradle and application.yml

--- backend/project1/src/test/java/com/team4/project1/CustomerTest.java
+++ backend/project1/src/test/java/com/team4/project1/CustomerTest.java
@@ -3,21 +3,14 @@ package com.team4.project1;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team4.project1.domain.customer.repository.CustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class customersTest {
+public class CustomerTest {
     @Autowired
     private MockMvc mockMvc;
 
