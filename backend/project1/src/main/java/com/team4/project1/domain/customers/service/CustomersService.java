d2efae6f22767298bc6f2f5a53ea615d7d54de14
sojunsik <chunsik1003@gmail.com>
2025-02-19 15:33:17 +0900

    feature CustomersService

--- backend/project1/src/main/java/com/team4/project1/domain/customers/service/CustomersService.java
+++ backend/project1/src/main/java/com/team4/project1/domain/customers/service/CustomersService.java
@@ -1,12 +1,18 @@
 package com.team4.project1.domain.customers.service;
 
+import com.team4.project1.domain.customers.dto.CustomersDto;
+import com.team4.project1.domain.customers.entity.Customers;
 import com.team4.project1.domain.customers.repository.CustomersRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class CustomersService {
+import java.util.List;
+import java.util.Optional;
 
+@Service
+@RequiredArgsConstructor
+public class CustomersService {
 
     private CustomersRepository customersRepository;
 
@@ -14,12 +20,23 @@ public class CustomersService {
         this.customersRepository = customersRepository;
     }
 
-    public
+    // Customers 객체 생성
+    public Customers createCustomer(CustomersDto customersDto) {
+        Customers customers = Customers.builder()
+                .name(customersDto.getName())
+                .email(customersDto.getEmail())
+                .build();
+        return customersRepository.save(customers);
+    }
 
-    // 회원정보 조회
-    @Transactional
-    public void checkinfo(Long id) {
+    // Cusotmers id 기반 조회
+    public Optional<Customers> getCustomersById(long id) {
+        return customersRepository.findById(id);
+    }
 
+    // Customers 전체 조회
+    public List<Customers> getAllCustomers() {
+        return customersRepository.findAll();
     }
 
 }
