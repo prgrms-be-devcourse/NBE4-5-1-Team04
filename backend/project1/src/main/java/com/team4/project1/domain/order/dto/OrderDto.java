0763ea870e21c93abcbbed26f051d6f45ae839ca
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-02-20 21:07:21 +0900

    feature: Implement DTOs for entities used in the order domain

--- backend/project1/src/main/java/com/team4/project1/domain/order/dto/OrderDto.java
+++ backend/project1/src/main/java/com/team4/project1/domain/order/dto/OrderDto.java
@@ -1,13 +1,12 @@
 package com.team4.project1.domain.order.dto;
 
-import com.team4.project1.domain.customer.dto.CustomerDto;
 import lombok.Getter;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Getter
 public class OrderDto {
     private Long id;
-    private Date date;
+    private LocalDateTime date;
     private Long totalPrice;
 }
--- backend/project1/src/main/java/com/team4/project1/domain/order/dto/OrderWithOrderItemsDto.java
+++ backend/project1/src/main/java/com/team4/project1/domain/order/dto/OrderWithOrderItemsDto.java
@@ -1,14 +1,26 @@
 package com.team4.project1.domain.order.dto;
 
+import com.team4.project1.domain.order.entity.Order;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class OrderWithOrderItemsDto {
     private Long id;
-    private Date date;
+    private LocalDateTime date;
     private Long totalPrice;
-    private List<OrderItemDto> orderItems;
+    private List<OrderItemDto> orderedItems;
+
+    public static OrderWithOrderItemsDto from(Order order) {
+        return new OrderWithOrderItemsDto(
+                order.getId(),
+                order.getDate(),
+                order.getTotalPrice(),
+                order.getOrderItems().stream().map(OrderItemDto::from).toList()
+        );
+    }
 }
