62e644f125eab93bbca1f2742a3e461ca845f2ac
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-02-21 10:42:46 +0900

    refactor: Improve .from() method logic

--- backend/project1/src/main/java/com/team4/project1/domain/item/dto/ItemDto.java
+++ backend/project1/src/main/java/com/team4/project1/domain/item/dto/ItemDto.java
@@ -1,23 +1,23 @@
 package com.team4.project1.domain.item.dto;
 
 import com.team4.project1.domain.item.entity.Item;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Locale;
+
 @Getter
+@AllArgsConstructor
 public class ItemDto {
     private Integer id;
     private String name;
     private Integer price;
 
-    public ItemDto(Item item) {
-        this.id = item.getId();
-        this.name = item.getName();
-        this.price = item.getPrice();
-    }
-
     public static ItemDto from(Item item) {
-        return new ItemDto(item);
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getPrice()
+        );
     }
-
-
 }
