d1effcb053ea6630735c8cfa8c8ed4dc9cfa2242
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-02-21 15:01:00 +0900

    merge: [API] HTTP 응답 표준화 (#36)
    * feature: Implement DTO for HTTP responses
    
    * feature: Wrap responses returned by the exception handler inside ResponseDTOs
    
    * feature: Wrap responses retured by the controller layer inside ResponseDTOs
    
    * fix: Instantiate ResponseDto through static method instead of constructor

--- backend/project1/src/main/java/com/team4/project1/global/exception/GlobalExceptionHandler.java
+++ backend/project1/src/main/java/com/team4/project1/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.team4.project1.global.exception;
 
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.team4.project1.global.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -10,53 +10,53 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     //고객 조회 시 존재하지 않는 ID 입력 (CustomerNotFoundException)
     @ExceptionHandler(CustomerNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleCustomerNotFoundException(CustomerNotFoundException ex, HttpServletRequest request) {
-        return createErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(),request.getRequestURI());
+    public ResponseEntity<ResponseDto<String>> handleCustomerNotFoundException(CustomerNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ResponseDto.notFound(ex.getMessage()));
     }
 
     //아이템 조회 시 존재하지 않는 ID 입력 (ItemNotFoundException)
     @ExceptionHandler(ItemNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleItemNotFoundException(ItemNotFoundException ex, HttpServletRequest request) {
-        return createErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(),request.getRequestURI());
+    public ResponseEntity<ResponseDto<String>> handleItemNotFoundException(ItemNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ResponseDto.notFound(ex.getMessage()));
     }
     //잘못된 요청 데이터 (MethodArgumentNotValidException)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
-        return createErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(),request.getRequestURI());
+    public ResponseEntity<ResponseDto<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ResponseDto.notFound(ex.getMessage()));
     }
+
     //잘못된 JSON 형식 (HttpMessageNotReadableException)
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex, HttpServletRequest request) {
-        return createErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(),request.getRequestURI());
+    public ResponseEntity<ResponseDto<String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ResponseDto.badRequest(ex.getMessage()));
     }
+
     //잘못된 HTTP 메서드 (HttpRequestMethodNotSupportedException)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex, HttpServletRequest request) {
-        return createErrorResponse(HttpStatus.METHOD_NOT_ALLOWED, ex.getMessage(),request.getRequestURI());
+    public ResponseEntity<ResponseDto<String>> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(ResponseDto.methodNotAllowed(ex.getMessage()));
     }
+
     //일반적인 예외 처리 (Exception)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex, HttpServletRequest request) {
-        return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(),request.getRequestURI());
-    }
-
-
-    //에러코드 생성 메서드
-    private ResponseEntity<ErrorResponse> createErrorResponse(HttpStatus status, String message, String path) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                LocalDateTime.now(),
-                status.value(),
-                status.getReasonPhrase(),
-                message,
-                path
-        );
-        return new ResponseEntity<>(errorResponse, status);
+    public ResponseEntity<ResponseDto<String>> handleGeneralException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ResponseDto.internalServerError(ex.getMessage()));
     }
 }
