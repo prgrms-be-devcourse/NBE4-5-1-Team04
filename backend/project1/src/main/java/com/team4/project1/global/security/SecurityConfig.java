949367c2cc151d1e85f7a476a78cfea5f48395b3
jxxngin <wwd0203@daum.net>
2025-02-21 10:40:43 +0900

    feat: [회원] security config (#25)

---
+++ backend/project1/src/main/java/com/team4/project1/global/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.team4.project1.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        (authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers(HttpMethod.GET,
+                                        "/api/v1/items",
+                                        "/api/v1/items/{itemId:\\d+}"
+                                )
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                );
+
+        return http.build();
+    }
+}
