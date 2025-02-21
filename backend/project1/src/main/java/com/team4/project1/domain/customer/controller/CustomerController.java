6a14452bbbe6a1f10e388092d755f3b669e4f05c
kozae00 <rhwogus777@gmail.com>
2025-02-21 15:15:06 +0900

    refactor: 컨트롤러에서 예외 처리 로직 제거

--- backend/project1/src/main/java/com/team4/project1/domain/customer/controller/CustomerController.java
+++ backend/project1/src/main/java/com/team4/project1/domain/customer/controller/CustomerController.java
@@ -8,7 +8,6 @@ import com.team4.project1.global.exception.CustomerNotFoundException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- backend/project1/src/main/java/com/team4/project1/global/dto/ResponseDto.java
+++ backend/project1/src/main/java/com/team4/project1/global/dto/ResponseDto.java
@@ -19,27 +19,29 @@ public class ResponseDto<T> {
         return new ResponseDto<>(code, message, data);
     }
 
-    // HTTP 응답코드별 static 메소드
-    // 2xx
+
+    // 2xx (성공 응답)
     public static <T> ResponseDto<T> ok(T data) {
         return new ResponseDto<>(HttpStatus.OK.value() + "", HttpStatus.OK.getReasonPhrase(), data);
     }
 
-    // 4xx
-    public static <T> ResponseDto<T> badRequest(T data) {
-        return new ResponseDto<>(HttpStatus.BAD_REQUEST.value() + "", HttpStatus.BAD_REQUEST.getReasonPhrase(), data);
+
+    // 4xx (클라이언트 오류 응답)
+    public static ResponseDto<String> badRequest(String message) {
+        return new ResponseDto<>(HttpStatus.BAD_REQUEST.value() + "", message, null);
     }
 
-    public static <T> ResponseDto<T> notFound(T data) {
-        return new ResponseDto<>(HttpStatus.NOT_FOUND.value() + "", HttpStatus.NOT_FOUND.getReasonPhrase(), data);
+    public static ResponseDto<String> notFound(String message) {
+        return new ResponseDto<>(HttpStatus.NOT_FOUND.value() + "", message, null);
     }
 
-    public static <T> ResponseDto<T> methodNotAllowed(T data) {
-        return new ResponseDto<>(HttpStatus.METHOD_NOT_ALLOWED.value() + "", HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase(), data);
+    public static ResponseDto<String> methodNotAllowed(String message) {
+        return new ResponseDto<>(HttpStatus.METHOD_NOT_ALLOWED.value() + "", message, null);
     }
 
-    // 5xx
-    public static <T> ResponseDto<T> internalServerError(T data) {
-        return new ResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR.value() + "", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), data);
+
+    // 5xx (서버 오류 응답)
+    public static ResponseDto<String> internalServerError(String message) {
+        return new ResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR.value() + "", message, null);
     }
 }
