ca2e4d6e74e455ddff405c8e56fac4242106f8ae
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-02-22 13:47:49 +0900

    feature: Implement static instantiation methods

--- backend/project1/src/main/java/com/team4/project1/domain/customer/dto/CustomerDto.java
+++ backend/project1/src/main/java/com/team4/project1/domain/customer/dto/CustomerDto.java
@@ -2,11 +2,12 @@ package com.team4.project1.domain.customer.dto;
 
 import com.team4.project1.domain.customer.entity.Customer;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDto {
 
     private Long id;
@@ -23,15 +24,22 @@ public class CustomerDto {
     @NotBlank(message = "email은 필수 입력 값입니다.")
     private String email;
 
-    public CustomerDto(Customer customer) {
-        this.id = customer.getId();
-        this.name = customer.getName();
-        this.email = customer.getEmail();
-    }
-
     public CustomerDto(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
+    public static CustomerDto from(Customer customer) {
+        return new CustomerDto(
+            customer.getId(),
+            customer.getUsername(),
+            customer.getPassword(),
+            customer.getName(),
+            customer.getEmail()
+        );
+    }
+
+    public static CustomerDto of(Long id, String username, String password, String name, String email) {
+        return new CustomerDto(id, username, password, name, email);
+    }
 }
