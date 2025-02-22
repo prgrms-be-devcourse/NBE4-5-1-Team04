2f1f392ed53d775ac1f82f2a4a4d24f8347ce464
sojunsik <chunsik1003@gmail.com>
2025-02-22 13:58:47 +0900

    feature/orderRepository에 특정시간범위 주문조회기능

--- backend/project1/src/main/java/com/team4/project1/domain/order/repository/OrderRepository.java
+++ backend/project1/src/main/java/com/team4/project1/domain/order/repository/OrderRepository.java
@@ -4,9 +4,12 @@ import com.team4.project1.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByCustomerId(Long customerId);
+
+    List<Order> findAllByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
