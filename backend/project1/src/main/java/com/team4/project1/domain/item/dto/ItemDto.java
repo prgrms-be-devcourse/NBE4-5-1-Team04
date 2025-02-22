c89a9ca58bf077db85c16ac5375107931778809c
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-02-22 14:15:37 +0900

    merge: [리팩토링] DTO 생성 메소드 (#47)
    * feature: Implement static instantiation methods
    
    * fix: Instantiate DTOs with static methods instead of constructor
    
    * task: Add suggestion for later

--- backend/project1/src/main/java/com/team4/project1/domain/item/dto/ItemDto.java
+++ backend/project1/src/main/java/com/team4/project1/domain/item/dto/ItemDto.java
@@ -24,4 +24,8 @@ public class ItemDto {
                 item.getPrice()
         );
     }
+
+    public static ItemDto of(Long id, String name, Integer price) {
+        return new ItemDto(id, name, price);
+    }
 }
