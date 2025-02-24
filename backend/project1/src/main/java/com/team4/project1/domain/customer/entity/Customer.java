8b8f8b4d11dc3ec6b8d6e1839846578dd9f40471
kozae00 <rhwogus777@gmail.com>
2025-02-24 09:42:24 +0900

    refactor: Customer 엔티티에서 PK 관리 방식 개선 및 변경 메서드 추가

--- backend/project1/src/main/java/com/team4/project1/domain/customer/entity/Customer.java
+++ backend/project1/src/main/java/com/team4/project1/domain/customer/entity/Customer.java
@@ -9,47 +9,56 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.NONE) // id 수정 불가능
+    @Setter(AccessLevel.NONE) // id는 절대 변경 불가
     private Long id;
 
-    @Column(length = 100, unique = true,nullable = false)
+    @Column(length = 100, unique = true, nullable = false)
     private String username;
 
-    @Column(length = 100,nullable = false)
+    @Column(length = 100, nullable = false)
     private String password;
 
     @Column(length = 100, unique = true)
     private String apiKey;
 
+    @Setter
     @Column(nullable = false)
     private String name;
 
+    @Setter
     @Column(nullable = false, unique = true)
     private String email;
 
-    @OneToMany(mappedBy = "customer",fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
     private List<Order> orders = new ArrayList<>();
 
-    public Customer(String username, String password, String name, String email) {
+    // Lombok Builder에서 id를 제외하고, 필요한 필드만 생성자로 받아서 엔티티 생성.
+    @Builder
+    public Customer(String username, String password, String name, String email, String apiKey) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.email = email;
+        this.apiKey = apiKey;
     }
 
-    public Customer(String username, String password) {
-        this.username = username;
-        this.password = password;
-        this.apiKey = username;
-    }
+//    public Customer(String username, String password, String name, String email) {
+//        this.username = username;
+//        this.password = password;
+//        this.name = name;
+//        this.email = email;
+//    }
+//
+//    public Customer(String username, String password) {
+//        this.username = username;
+//        this.password = password;
+//        this.apiKey = username;
+//    }
 
     public boolean isAdmin() {
         return username.equals("admin");
