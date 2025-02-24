af4390e8e75fec03d56c0b6e7aee017d88ce7aa0
jxxngin <wwd0203@daum.net>
2025-02-24 18:02:47 +0900

    docs: order controller

--- backend/project1/src/main/java/com/team4/project1/domain/order/controller/ApiV1OrderController.java
+++ backend/project1/src/main/java/com/team4/project1/domain/order/controller/ApiV1OrderController.java
@@ -3,17 +3,17 @@ package com.team4.project1.domain.order.controller;
 import com.team4.project1.domain.order.dto.OrderDto;
 import com.team4.project1.domain.order.dto.OrderItemDto;
 import com.team4.project1.domain.order.dto.OrderWithOrderItemsDto;
-import com.team4.project1.domain.order.entity.Order;
 import com.team4.project1.domain.order.service.OrderService;
 import com.team4.project1.global.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+@Tag(name = "ApiV1CustomerController", description = "주문 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/orders")
@@ -21,6 +21,7 @@ public class ApiV1OrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "주문 생성")
     @PostMapping
     public ResponseEntity<ResponseDto<OrderWithOrderItemsDto>> createOrder(
             @RequestParam("cust_id") Long customerId,
@@ -28,23 +29,32 @@ public class ApiV1OrderController {
         return ResponseEntity.ok(ResponseDto.ok(orderService.createOrder(orderItemDtos, customerId)));
     }
 
-
+    @Operation(summary = "주문 수정")
     @PutMapping("/{orderId}")
     public ResponseEntity<ResponseDto<OrderWithOrderItemsDto>> updateOrder(@PathVariable Long orderId, @RequestBody List<OrderItemDto> orderItemDtos) {
         return ResponseEntity.ok(ResponseDto.ok(orderService.updateOrder(orderItemDtos, orderId)));
     }
 
+    @Operation(summary = "주문 삭제")
     @DeleteMapping("/{orderId}")
     public ResponseEntity<ResponseDto<Long>> cancelOrder(@PathVariable Long orderId) {
         return ResponseEntity.ok(ResponseDto.ok(orderService.cancelOrder(orderId)));
     }
 
+    @Operation(
+            summary = "주문 불러오기",
+            description = "orderId를 통해 특정 주문 불러오기"
+    )
     @GetMapping("/{orderId}")
     public ResponseEntity<ResponseDto<OrderWithOrderItemsDto>> getOrderByOrderId(
             @PathVariable Long orderId) {
         return ResponseEntity.ok(ResponseDto.ok(orderService.getOrderById(orderId)));
     }
 
+    @Operation(
+            summary = "특정 회원의 전체 주문 불러오기",
+            description = "회원 id 값을 통해 특정 회원에 대한 주문 전체 불러오기"
+    )
     @GetMapping(value = "", params = "cust_id")
     public ResponseEntity<ResponseDto<List<OrderDto>>> getAllOrders(
             @RequestParam("cust_id") Long customerId) {
