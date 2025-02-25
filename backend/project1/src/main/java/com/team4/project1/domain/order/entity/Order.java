9931fc24abe9a458060becf04c4d7ece23e9b10a
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-02-25 11:26:03 +0900

    feature: Make new orders start off as temporary

--- backend/project1/src/main/java/com/team4/project1/domain/order/entity/Order.java
+++ backend/project1/src/main/java/com/team4/project1/domain/order/entity/Order.java
@@ -64,14 +64,14 @@ public class Order {
 
     /**
      * 주문의 배송 상태
-     * {@link DeliveryStatus} 열거형을 사용하여 주문의 현재 상태를 나타냅니다
+     * {@link OrderStatus} 열거형을 사용하여 주문의 현재 상태를 나타냅니다
      * {@code PROCESSING}: 주문이 처리 중인 상태를 의미합니다.
      * {@code SHIPPED}: 주문이 배송된 상태를 의미합니다
      */
     @Setter
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private DeliveryStatus deliveryStatus = DeliveryStatus.PROCESSING;
+    private OrderStatus orderStatus = OrderStatus.TEMPORARY;
 
     /**
      * 새로운 주문을 생성하는 생성자입니다.
@@ -86,17 +86,17 @@ public class Order {
     }
 
     @Builder
-    public Order(Long id, Customer customer, LocalDateTime date, Long totalPrice, DeliveryStatus deliveryStatus) {
+    public Order(Long id, Customer customer, LocalDateTime date, Long totalPrice, OrderStatus orderStatus) {
         this.id = id;
         this.customer = customer;
         this.date = date;
         this.totalPrice = totalPrice;
-        this.deliveryStatus = deliveryStatus != null ? deliveryStatus : DeliveryStatus.PROCESSING;
+        this.orderStatus = orderStatus != null ? orderStatus : OrderStatus.TEMPORARY;
     }
 
     // 주문 정보 변경 메서드 추가
-    public void updateOrder(Long totalPrice, DeliveryStatus deliveryStatus) {
+    public void updateOrder(Long totalPrice, OrderStatus orderStatus) {
         this.totalPrice = totalPrice;
-        this.deliveryStatus = deliveryStatus;
+        this.orderStatus = orderStatus;
     }
 }
